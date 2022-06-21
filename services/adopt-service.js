f2f53e16a4b1e23d7a43908b84197000325a7479
Allison Ause <allison.ause@gmail.com>
2022-06-21 16:51:18 -0700

    Fleshed out adopt-service.js functionality

--- services/adopt-service.js
+++ services/adopt-service.js
@@ -3,6 +3,13 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
+    const response = await client
+        .from('dogs')
+        .select(`
+            id,
+            name,
+            breed
+        `);
     // *** from the dogs table, select all items
     // but you only need id, name, and breed columns
 
@@ -12,6 +19,11 @@ export async function getDogs() {
 }
 
 export async function getDog(id) {
+    const response = await client
+        .from('dogs')
+        .select()
+        .match({ id: id })
+        .single();
     // *** from the dogs table, select a >>single<< dog who has the >>matching id<<
     // return all columns
 
